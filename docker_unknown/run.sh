f1921d651f7ac98975ef5316c73a70027689f710
cyc <ycpss91255@gmail.com>
2023-03-22 10:26:12 +0800

    done: tutorial v1

--- docker_unknown/run.sh
+++ docker_unknown/run.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+WS_PATH=/home/iclab/workspace/docker_tutorial_ws/
+
+user=$(id -un)
+
 docker run --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -e DISPLAY=${DISPLAY} \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /dev:/dev \
+    --gpus all \
+    -v ${WS_PATH}:/home/${user}/work \
     -it test
